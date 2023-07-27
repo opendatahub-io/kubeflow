eddf82b0d589fb51745379bc386350f3a3c8b199
atheo89 <atheodorak@outlook.com>
2023-07-27 13:42:39 +0200

    Revert to the original configs

--- components/odh-notebook-controller/controllers/notebook_route.go
+++ components/odh-notebook-controller/controllers/notebook_route.go
@@ -41,7 +41,7 @@ func NewNotebookRoute(notebook *nbv1.Notebook) *routev1.Route {
 			},
 		},
 		Spec: routev1.RouteSpec{
-			Path: "/notebook/" + notebook.Namespace + "/" + notebook.Name,
+			// Path: "/notebook/" + notebook.Namespace + "/" + notebook.Name,
 			To: routev1.RouteTargetReference{
 				Kind:   "Service",
 				Name:   notebook.Name,
